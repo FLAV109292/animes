2d178a11ec686db2c9fd4a3fe378deeb6f935df2
FLAV109292 <0000109292176xsp@al.educacao.sp.gov.br>
2024-11-08 10:59:58 -0300

    Add files via upload

---
+++ mangas-site-2 - Copia/flashcard-aula_10/flashcard-aula_10/perguntas.js
@@ -0,0 +1,93 @@
+criaCartao(
+    'Code Geass',
+    'Quem é o protagonista de Code Geass?',
+    ' protagonista é Lelouch vi Britannia, também conhecido como Lelouch Lamperouge'
+)
+
+criaCartao(
+    'cavaleiros do zodiaco',
+    'quantos cavaleiros de ouro tem?',
+    'a 12 cavaleiros de ouro que representão os 12 signos do zodiaco'
+)
+
+criaCartao(
+    'Demon Sleyer',
+    'quais os nomes dos protagonistas?',
+    'são Tanjirō Kamado, Nezuko Kamado, Zenitsu Agatsuma e Inosuke Hashibira'   
+ 
+)
+criaCartao(
+    'Code Geass',
+    'Qual é o poder que Lelouch adquire em Code Geass?',
+    'Lelouch recebe o poder do Geass , que lhe permite dar ordens absolutas que qualquer pessoa seja obrigada a obedecer, desde que contato visual.'
+)
+criaCartao(
+    'cavaleiros do zodiaco',
+    'Quais são os Cavaleiros de Ouro?',
+    'Mu de Áries, Aldebaran de Touro, Saga de Gêmeos, Máscara da Morte de Câncer, Aiolia de Leão, Shaka de Virgem, Dohko de Libra, Milo de Escorpião, Aiolos de Sagitário, Shura de Capricórnio, Camus de Aquário, Afrodite de Peixes'
+)
+criaCartao(
+    'Demon Sleyer',
+    'qual a posição mais alta entre os caçadores?',
+    'são os hashiras'
+)
+criaCartao(
+    'Code Geass',
+    'O que são os Cavaleiros Negroso?',
+    'Os Cavaleiros Negros são uma organização militar criada e liderada por Lelouch, sob a identidade de Zero, com o objetivo de derrubar o Império da Britânia.'
+)
+criaCartao(
+    'cavaleiros do zodiaco',
+    'Qual é a importância das armaduras nos Cavaleiros do Zodíaco?',
+    'As armaduras, ou “Cloths”, são fundamentais para os Cavaleiros, protegendo-os e aumentando seu poder. Ligadas às constelações, podem ser restauradas, destruídas e evoluídas, como as de Bronze, que podem atingir a forma divina, como Armaduras de Deus'
+)
+criaCartao(
+    'Demon Sleyer',
+    'quantos hashiras tem e quais são seus  nomes?',
+    'a 9 hashiras seus nomes são Gyomei Himejima, Sanemi Shinazugawa, Giyu Tomioka, Muichiro Tokito, Obanai Iguro, Kyojuro Rengoku, Mitsuri Kanroji, Tengen Uzui e Shinobu Kocho'
+)
+criaCartao(
+    'naruto',
+    'O que é um Hokage?',
+    'Hokage é o título dado ao líder da Vila da Folha (Konohagakure), sendo o ninja mais forte e respeitado.'
+)
+criaCartao(
+    'Jujutsu Kaisen',
+    'Quem é Satoru Gojo e qual é sua importância na história?',
+    'Satoru Gojo é o professor de Yuji e um dos feiticeiros de jujutsu mais fortes. Ele é conhecido por suas habilidades incomparáveis ​​e por ser o principal protetor dos alunos da Escola de Jujutsu'
+)
+criaCartao(
+    'Yu-Gi-Oh!',
+    'quais as funções mais fortes de Yu-Gi-Oh!',
+    'Invocação-Sincro, Invocação-XYZ, Invocação-Link, Invocação-Fusão, Efeitos de Negação, Destruição em Massa, Efeitos de Banimento, Cartas de Destruição Direta e Efeitos de Invocação Especial'
+)
+criaCartao(
+    'naruto',
+    'O que são os Jinchuuriki?',
+    'Jinchuuriki são ninjas que têm bestas com caudas (Bijuu) seladas dentro deles. Essas bestas possuem grande poder, e o portador pode usá-lo em combate. Naruto é o Jinchuuriki da Nove-Caudas, Kurama.'
+)
+criaCartao(
+    'Jujutsu Kaisen',
+    'Quem é o protagonista de Jujutsu Kaisen?',
+    'O protagonista é Yuji Itadori , um adolescente que possui força física incrível e acaba se tornando um receptáculo para Sukuna, o "Rei das Maldições'
+)
+criaCartao(
+    'Yu-Gi-Oh!',
+    'Quem são os personagens principais do anime original de Yu-Gi-Oh!?',
+    'Yugi Mutou, Atem, Seto Kaiba e Joey Wheeler'
+)
+criaCartao(
+    'naruto',
+    'Quem são os Hokages?',
+    'Hashirama Senju(Primeiro Hokage), Tobirama Senju(Segundo Hokage), Hiruzen Sarutobi(Terceiro Hokage), Minato Namikaze(Quarto Hokage), Tsunade(Quinta Hokage), Kakashi Hatake(Sexto Hokage), Naruto Uzumaki(Sétimo Hokage)'
+)
+criaCartao(
+    'Jujutsu Kaisen',
+    'Quem é Ryomen Sukuna?',
+    'sukuna é um espírito amaldiçoado extremamente poderoso e cruel, conhecido como "Rei das Maldições". Ele habita o corpo de Yuji Itadori após este ingerir um de seus dedos.'
+)
+criaCartao(
+    'Yu-Gi-Oh!',
+    'O que significa Tributo em Yu-Gi-Oh!?',
+    'Um tributo é o ato de sacrificar monstros no campo para invocar monstros mais poderosos. Monstros de Nível 5 ou 6 desabilitam um tributo, e monstros de Nível 7 ou superior desabilitam dois tributos para serem invocados.'
+)
